a15d5ce3ccb05f5f9689ab02bbd04e884d909ee7
nakagawa.shota <nakagawa.shota@HL00416.local>
2020-12-27 21:01:51 +0900

    fix ghq directory

--- dotfilelink.sh
+++ dotfilelink.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 
-DOTFILES_DIR=${HOME}/.ghq/github.com/snakagawax/dotfiles
+DOTFILES_DIR=${HOME}/ghq/github.com/snakagawax/dotfiles
  
 ln -fs ${DOTFILES_DIR}/.bashrc ${HOME}
 ln -fs ${DOTFILES_DIR}/.bash_profile ${HOME}
